5809678ff4970ee42540b59d6ecca6d61eba8a3f
Taq Karim <taq.karim@oracle.com>
2020-07-21 00:47:44 -0400

    added comments

--- airtable.go
+++ airtable.go
@@ -1,3 +1,5 @@
+// Package airtable provides a simple client for interacting
+// with the Airtable API.
 package airtable
 
 import (
@@ -19,6 +21,8 @@ type GenericTable struct {
 	View    string
 }
 
+// NewTable returns a GenericTable struct that implements
+// the Table interface
 func NewTable(name string, account Account) Table {
 	return &GenericTable{
 		account: &account,
@@ -27,11 +31,13 @@ func NewTable(name string, account Account) Table {
 	}
 }
 
+// List returns a list of records from the Airtable.
 func (t *GenericTable) List(opts Options) ([]Record, error) {
 	log.Printf("calling list")
 	return []Record{}, nil
 }
 
+// Update makes a PATCH request to all records provided to Airtable.
 func (t *GenericTable) Update(recs []Record) error {
 	log.Printf("calling update")
 	return nil
